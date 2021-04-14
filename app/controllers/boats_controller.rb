61de66e78772ced333c9eee42a9b6296bd5a35dd
Micah Sterling <msterling89@gmail.com>
2021-04-13 20:48:20 -0500

    show action w/navigation

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -3,4 +3,9 @@ class BoatsController < ApplicationController
     @boats = Boat.all
     render 'index.html.erb'
   end
+
+  def show
+    @boat = Boat.find_by(id: params[:id])
+    render 'show.html.erb'
+  end
 end
